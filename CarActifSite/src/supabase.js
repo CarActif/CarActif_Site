37b66b84c88547b6a2767bbd567078fd0948439e
CarActif <contactcharlauto@gmail.com>
2025-07-17 01:14:04 +0200

    Delete CarActifSite directory

--- CarActifSite/src/supabase.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://rhbnlcmsyqjbzykexttt.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJoYm5sY21zeXFqYnp5a2V4dHR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA0NDA5OTcsImV4cCI6MjA2NjAxNjk5N30.o24Li47McJpMVaa2sEjwR8oOa1mobsUqhdUUD2VwpT8";
-
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
-export default supabase;
